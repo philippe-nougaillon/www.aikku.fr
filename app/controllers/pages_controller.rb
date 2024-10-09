6fef5ff5c6806b85a248ba08557c427b299e1f1d
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2024-10-09 14:48:42 +0200

    small fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,33 +1,27 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def home
-    @projets = Projet.all
-    @projets_commits = @projets.sum(:commit)
-    @projets_deploys = @projets.sum(:deploy)
-    @projets_coffees = @projets.sum(:coffee)
-  end
+  def home; end
 
-  def portfolio
-    @projets = Projet.all
-    @tags = @projets.tag_counts_on(:tags).order(:taggings_count).reverse
+  # def portfolio
+  #   @projets = Projet.all
+  #   @tags = @projets.tag_counts_on(:tags).order(:taggings_count).reverse
 
-    unless params[:tag].blank? 
-      if params[:tag] != session[:tag]
-        @projets = Projet.tagged_with(params[:tag])
-        session[:tag] = params[:tag]
-      else
-        session[:tag] = params[:tag] = nil
-      end
-    end
-    @projets_commits = @projets.sum(:commit)
-    @projets_deploys = @projets.sum(:deploy)
-    @projets_coffees = @projets.sum(:coffee)
-    @projets = @projets.includes(:tags)
-  end
+  #   unless params[:tag].blank?
+  #     if params[:tag] != session[:tag]
+  #       @projets = Projet.tagged_with(params[:tag])
+  #       session[:tag] = params[:tag]
+  #     else
+  #       session[:tag] = params[:tag] = nil
+  #     end
+  #   end
+  #   @projets_commits = @projets.sum(:commit)
+  #   @projets_deploys = @projets.sum(:deploy)
+  #   @projets_coffees = @projets.sum(:coffee)
+  #   @projets = @projets.includes(:tags)
+  # end
 
-  def contact
-  end
+  def contact; end
 
   def contact_submit
     if verify_recaptcha
@@ -35,70 +29,54 @@ class PagesController < ApplicationController
       ContactMailer.submitted(message).deliver_now
       redirect_to root_path, notice: 'Votre message a bien été envoyé.'
     else
-      flash[:alert] = "Problème avec reCAPTCHA, merci de réessayer"
+      flash[:alert] = 'Problème avec reCAPTCHA, merci de réessayer'
       render 'contact'
     end
   end
 
-  def qui_sommes_nous
-  end
+  def qui_sommes_nous; end
 
-  def blog
-    @posts = Post.where(published: true)
-    @tags = @posts.tag_counts_on(:tags).order(:taggings_count).reverse
+  # def blog
+  #   @posts = Post.where(published: true)
+  #   @tags = @posts.tag_counts_on(:tags).order(:taggings_count).reverse
 
-    unless params[:tag].blank?
-      if params[:tag] != session[:tag]
-        @posts = Post.tagged_with(params[:tag])
-        session[:tag] = params[:tag]
-      else
-        session[:tag] = params[:tag] = nil
-      end
-    end
-  end
+  #   return if params[:tag].blank?
+
+  #   if params[:tag] != session[:tag]
+  #     @posts = Post.tagged_with(params[:tag])
+  #     session[:tag] = params[:tag]
+  #   else
+  #     session[:tag] = params[:tag] = nil
+  #   end
+  # end
 
   def nos_clients
     @clients = Client.all
     @tags = @clients.tag_counts_on(:tags).order(:taggings_count).reverse
 
-    unless params[:tag].blank? 
-      if params[:tag] != session[:tag]
-        @clients = Client.tagged_with(params[:tag])
-        session[:tag] = params[:tag]
-      else
-        session[:tag] = params[:tag] = nil
-      end
+    return if params[:tag].blank?
+
+    if params[:tag] != session[:tag]
+      @clients = Client.tagged_with(params[:tag])
+      session[:tag] = params[:tag]
+    else
+      session[:tag] = params[:tag] = nil
     end
   end
 
-  def services
-  end
+  def services; end
 
-  # def rechercher
-  #   unless params[:search].blank?
-  #     @results = PgSearch.multisearch("%#{ params[:search] }%")
-  #   end
-  # end
+  def logiciels; end
 
-  def logiciels
-  end
+  def camino; end
 
-  def camino
-  end
+  def prestofacto; end
 
-  def prestofacto
-  end
+  def planning; end
 
-  def planning
-  end
+  def guide_planning; end
 
-  def guide_planning
-  end
+  def mentions_légales; end
 
-  def mentions_légales
-  end
-
-  def crystal_data
-  end
-
+  def crystal_data; end
 end
