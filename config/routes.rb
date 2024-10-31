c7ad45da59f6897c91cbf928a90f7c950451d67d
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-10-31 16:46:55 +0100

    Internationalization done

--- config/routes.rb
+++ config/routes.rb
@@ -3,32 +3,34 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :clients
-  # resources :projets
-  # resources :posts
+  scope '(:locale)', locale: /en|fr/ do
+    resources :clients
+    # resources :projets
+    # resources :posts
 
-  controller :pages do
-    get :home, to: 'pages#home'
-    get :aikku_access, to: 'pages#aikku_access'
-    get :aikku_sign, to: 'pages#aikku_sign'
-    get :aikku_projects, to: 'pages#aikku_projects'
-    get :aikku_plann, to: 'pages#aikku_plann'
+    controller :pages do
+      get :home, to: 'pages#home'
+      get :aikku_access, to: 'pages#aikku_access'
+      get :aikku_sign, to: 'pages#aikku_sign'
+      get :aikku_projects, to: 'pages#aikku_projects'
+      get :aikku_plann, to: 'pages#aikku_plann'
 
-    get :qui_sommes_nous, to: 'pages#qui_sommes_nous'
-    get :nos_clients, to: 'pages#nos_clients'
-    get :services, to: 'pages#services'
-    get :logiciels, to: 'pages#logiciels'
-    get :contact, to: 'pages#contact'
-    post :contact, to: 'pages#contact_submit'
-    get :mentions_légales, to: 'pages#mentions_légales', as: :mentions_legales
+      get :qui_sommes_nous, to: 'pages#qui_sommes_nous'
+      get :nos_clients, to: 'pages#nos_clients'
+      get :services, to: 'pages#services'
+      get :logiciels, to: 'pages#logiciels'
+      get :contact, to: 'pages#contact'
+      post :contact, to: 'pages#contact_submit'
+      get :mentions_légales, to: 'pages#mentions_légales', as: :mentions_legales
 
-    get :prestofacto, to: 'pages#prestofacto'
-    get :talea, to: 'pages#talea'
+      get :prestofacto, to: 'pages#prestofacto'
+      get :talea, to: 'pages#talea'
 
-    # get :portfolio, to: 'pages#portfolio'
-    # get :blog, to: 'pages#blog'
-    # get :guide_planning, to: 'pages#guide_planning'
+      # get :portfolio, to: 'pages#portfolio'
+      # get :blog, to: 'pages#blog'
+      # get :guide_planning, to: 'pages#guide_planning'
+    end
+
+    root 'pages#home'
   end
-
-  root 'pages#home'
 end
