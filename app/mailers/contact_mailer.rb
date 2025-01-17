530f7e83238ac75e35e33bc001c97c0f78b46d92
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2025-01-17 16:02:17 +0100

    design / devise routes removed

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
   def submitted(message)
     @message = message
-    mail(to: 'contact@aikku.eu',
+    mail(to: 'sebastien.pourchaire@aikku.eu, philippe.nougaillon@aikku.eu, pierre-emmanuel.dacquet@aikku.eu',
          subject: '[WWW.AIKKU.EU] Nouveau contact')
   end
 end
