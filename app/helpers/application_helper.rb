2a4fd15515d53d399660e95fbf88ce37c001aa5d
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2025-01-22 12:11:21 +0100

    new screenshots for ACCESS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,39 +12,4 @@ module ApplicationHelper
         </li>
     ))
   end
-
-  class CodeRayify < Redcarpet::Render::HTML
-    def block_code(code, language)
-      CodeRay.scan(code, language).div
-    end
-  end
-
-  # Create a custom renderer that sets a custom class for block-quotes.
-  class CustomRender < Redcarpet::Render::HTML
-    def header(text, header_level)
-      %(<h#{header_level} class="font-black text-#{3 - header_level}xl my-6">#{text}</h#{header_level}>)
-    end
-
-    def block_code(code, _language)
-      %(<p class="bg-slate-100 p-3 my-3"><code>#{code}</code></p>)
-    end
-
-    def link(link, _title, content)
-      %(<a href="#{link}" class="underline" target="_blank">#{content}</a>)
-    end
-  end
-
-  def markdown(text)
-    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
-    options = {
-      fenced_code_blocks: true,
-      no_intra_emphasis: true,
-      autolink: true,
-      lax_html_blocks: true
-    }
-    # markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-    # markdown_to_html.render(text).html_safe
-    markdown_to_html = Redcarpet::Markdown.new(CustomRender, options)
-    markdown_to_html.render(text).html_safe
-  end
 end
