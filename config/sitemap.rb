6496c25d2cfe87355b8eb0e726f44414c581c49d
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2025-06-18 16:56:29 +0200

    useless routes removed

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,18 +4,18 @@ SitemapGenerator::Sitemap.default_host = 'https://www.aikku.eu/'
 SitemapGenerator::Sitemap.create do
   add aikku_access_path
   add aikku_plann_path
-  add aikku_projects_path
-  add aikku_sign_path
+  # add aikku_projects_path
+  # add aikku_sign_path
 
   add qui_sommes_nous_path
-  add logiciels_path
-  add services_path
-  add nos_clients_path
+  # add logiciels_path
+  # add services_path
+  # add nos_clients_path
   add contact_path
   add mentions_legales_path
 
-  add prestofacto_path
-  add talea_path
+  # add prestofacto_path
+  # add talea_path
   # add portfolio_path
   # add blog_path
 
