a11113ebb52cbac74b172a2197230c078a161f06
Dani Isaza <dani_isaza@hotmail.com>
2026-04-28 16:19:43 +0200

    "Corriger les warnings"

--- config/routes.rb
+++ config/routes.rb
@@ -13,20 +13,18 @@ Rails.application.routes.draw do
       get :aikku_access, to: "pages#aikku_access"
       get :aikku_plann, to: "pages#aikku_plann"
       get :aikku_coopcomm, to: "pages#aikku_coopcomm"
-
+      get :qui_sommes_nous, to: "pages#qui_sommes_nous"
+      get :nos_clients, to: "pages#nos_clients"
+      get :services, to: "pages#services"
+      get :contact, to: "pages#contact"
+      post :contact, to: "pages#contact_submit"
+      get :mentions_légales, to: "pages#mentions_légales", as: :mentions_legales
+      
       # get :aikku_projects, to: 'pages#aikku_projects'
       # get :aikku_sign, to: 'pages#aikku_sign'
       # get :prestofacto, to: 'pages#prestofacto'
       # get :talea, to: 'pages#talea'
-
-      get :qui_sommes_nous, to: "pages#qui_sommes_nous"
-      get :nos_clients, to: "pages#nos_clients"
-      get :services, to: "pages#services"
       # get :logiciels, to: 'pages#logiciels'
-      get :contact, to: "pages#contact"
-      post :contact, to: "pages#contact_submit"
-      get :mentions_légales, to: "pages#mentions_légales", as: :mentions_legales
-
       # get :portfolio, to: 'pages#portfolio'
       # get :blog, to: 'pages#blog'
       # get :guide_planning, to: 'pages#guide_planning'
