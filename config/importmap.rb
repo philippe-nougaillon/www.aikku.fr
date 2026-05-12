580c0bce045164c169cc5469ec829cc398f81167
Dani Isaza <dani_isaza@hotmail.com>
2026-05-12 17:19:55 +0200

    "faire la mise en page correctement"

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
-# Pin npm packages by running ./bin/importmap
-
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
 pin_all_from "app/javascript/controllers", under: "controllers"
+
 pin "stimulus-notification", to: "https://ga.jspm.io/npm:stimulus-notification@2.0.0/dist/stimulus-notification.es.js"
-pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
-pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.9.4/dist/hotkeys.esm.js"
-pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.50.0/dist/index.js"
-pin "stimulus-animated-number", to: "https://ga.jspm.io/npm:stimulus-animated-number@4.1.0/dist/stimulus-animated-number.mjs"
