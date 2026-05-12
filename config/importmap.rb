d2aca68f33c42384a5d74053a949e1577dcc606f
Dani Isaza <dani_isaza@hotmail.com>
2026-05-12 18:01:01 +0200

    "eviter l'erreur de type TypeError"

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "stimulus-notification", to: "https://ga.jspm.io/npm:stimulus-notification@2.0.0/dist/stimulus-notification.es.js"
+pin "stimulus-animated-number", to: "https://ga.jspm.io/npm:stimulus-animated-number@4.1.0/dist/stimulus-animated-number.mjs"
